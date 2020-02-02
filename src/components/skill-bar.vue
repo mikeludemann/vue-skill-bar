21a10a112eb3e70492e916436c398dc12c713d2b
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-02 16:18:02 +0100

    Setup complete - vue-skill-bar

---
+++ src/components/skill-bar.vue
@@ -0,0 +1,77 @@
+<template lang="html">
+
+	<section class="skill-bar">
+		<div :class="[classname,'container']">
+			<div>{{this.text}}</div>
+			<div class="container" :style="container">
+				<div class="skill--level" :style="skillLevel">{{this.size + " %"}}</div>
+			</div>
+		</div>
+	</section>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'skillbar',
+		props: {
+			classname: {
+				type: String,
+				required: false
+			},
+			text: {
+				type: String,
+				required: false
+			},
+			size: {
+				type: Number,
+				required: true
+			},
+			color: {
+				type: String,
+				required: false
+			},
+			bgcolor: {
+				type: String,
+				required: false
+			}
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+			container: function() {
+				return {
+					backgroundColor: "#eee",
+					width: "100%"
+				};
+			},
+			skillLevel: function() {
+				return {
+					color: this.color || "#000",
+					backgroundColor: this.bgcolor || "#fff",
+					width: this.size + "%",
+					textAlign: "left",
+					padding: "10px 10px"
+				};
+			}
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.skill-bar {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+    <skillbar text="IT" color="#ddd" bgcolor="#080" :size="80"/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import skillbar from '@/components/skill-bar.vue'
+
+export default {
+	name: 'home',
+	components: {
+		HelloWorld,
+		skillbar
+	}
+}
+</script>
